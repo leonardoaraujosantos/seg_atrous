ff94bd332b6c112a6f7528926a591eaae43a96dc
root <root@d48c1f6c-6c9b-4b90-a3dd-7cebf9f10ddc.apple.com>
2019-05-29 20:36:58 +0000

    Fix some small bugs on hyperparameter optimization

--- seg_models.py
+++ seg_models.py
@@ -83,6 +83,7 @@ class AtrousSeg(nn.Module):
         # Better for MSE
         #return result
         # Better for BCEWithLogitsLoss
+        return torch.sigmoid(result)
         return F.sigmoid(result)
         #return result
         #return self.model(x)
